62a8e03debc2ee334158fa2af344810f642b968a
pctn0007 <43162124+pctn0007@users.noreply.github.com>
2019-02-01 14:02:38 -0500

    Update firebaseConnection.py

--- Hardware_Code/test_scripts/firebaseConnection.py
+++ Hardware_Code/test_scripts/firebaseConnection.py
@@ -3,6 +3,6 @@ from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://motus-e3989.firebaseio.com/', None)
 
-result = firebase.post('/coupons',{'ID':random.randrange(100000, 999999, 6)})
+result = firebase.post('/random',{'ID':random.randrange(100000, 999999, 6)})
 
 print(result)
