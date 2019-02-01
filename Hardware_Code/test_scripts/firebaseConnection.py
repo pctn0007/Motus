c5cf225d1a8377ddf73e1e580f905f992520fdcb
pctn0007 <43162124+pctn0007@users.noreply.github.com>
2019-02-01 13:58:53 -0500

    Add files via upload

---
+++ Hardware_Code/test_scripts/firebaseConnection.py
@@ -0,0 +1,8 @@
+import random
+from firebase import firebase
+
+firebase = firebase.FirebaseApplication('https://motus-e3989.firebaseio.com/', None)
+
+result = firebase.post('/coupons',{'ID':random.randrange(100000, 999999, 6)})
+
+print(result)
