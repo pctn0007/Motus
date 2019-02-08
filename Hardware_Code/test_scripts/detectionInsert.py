ce519904d52466341125e8c4504ee074a2ea24de
pctn0007 <43162124+pctn0007@users.noreply.github.com>
2019-02-08 16:17:26 -0500

    Update detectionInsert.py

--- Hardware_Code/test_scripts/detectionInsert.py
+++ Hardware_Code/test_scripts/detectionInsert.py
@@ -5,25 +5,14 @@ import time
 from firebase import firebase
 from time import gmtime, strftime
 
-#GPIO settings
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(4,GPIO.IN)
-GPIO.output(17,GPIO.LOW)
-
 # Database root
 firebase = firebase.FirebaseApplication('https://motus-e3989.firebaseio.com/', None)
 
 # Database Detection data
-boxid = 'm1'
-udid =  random.randrange(100000, 999999, 6)
-pic = 'test.jpg'
-dtime = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-
-
-# MotionSensor detection
-
+boxid = 'm1'   # unique motus bo id
+uid =  'u1234' # unique user id via users table
+pic = 'test-snapsot.jpg' # snapshot file name
+dtime = strftime("%d %b %Y %H:%M:%S +0000", gmtime()) # detection timestamp (formatted)
 
 # JSON object to INSERT
 data = {'Uid': uid,
