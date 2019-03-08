b184bcc9cf139c1a96271161aa068baa2e7cf52c
LosesonWinson <43183468+LosesonWinson@users.noreply.github.com>
2019-03-08 15:09:38 -0500

    Add files via upload

---
+++ Research/test1.py
@@ -0,0 +1,19 @@
+# Import gcloud
+from google.cloud import storage
+import os
+credential_path = "c:\example.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+# Enable Storage
+client = storage.Client()
+
+# Reference an existing bucket.
+bucket = client.get_bucket('exampleprog-69f69.appspot.com')
+
+# Upload a local file to a new file to be created in your bucket.
+zebraBlob = bucket.blob('donk.jpg')
+zebraBlob.upload_from_filename(filename='donk.jpg')
+
+# Download a file from your bucket.
+#giraffeBlob = bucket.get_blob('giraffe.jpg')
+#giraffeBlob.download_as_string()
+#In line client = storage.Client()
