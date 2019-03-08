01d7b371cbac41b212b74db056acf253e7880b2e
LosesonWinson <43183468+LosesonWinson@users.noreply.github.com>
2019-03-08 15:17:22 -0500

    Update test1.py

--- Research/test1.py
+++ Research/test1.py
@@ -1,6 +1,7 @@
 # Import gcloud
 from google.cloud import storage
 import os
+# location of json file created
 credential_path = "c:\example.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 # Enable Storage
@@ -9,6 +10,7 @@ client = storage.Client()
 # Reference an existing bucket.
 bucket = client.get_bucket('exampleprog-69f69.appspot.com')
 
+#image has to be the same location as python script
 # Upload a local file to a new file to be created in your bucket.
 zebraBlob = bucket.blob('donk.jpg')
 zebraBlob.upload_from_filename(filename='donk.jpg')
