7766f9b27d55a67aab23a714d2aa4539bbe6ee91
pctn0007 <43162124+pctn0007@users.noreply.github.com>
2019-03-15 10:33:07 -0400

    Update test1.py

--- Research/test1.py
+++ Research/test1.py
@@ -2,18 +2,19 @@
 from google.cloud import storage
 import os
 # location of json file created
-credential_path = "c:\example.json"
+credential_path = "c:\motus.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 # Enable Storage
 client = storage.Client()
 
 # Reference an existing bucket.
-bucket = client.get_bucket('exampleprog-69f69.appspot.com')
+bucket = client.get_bucket('motus-e3989.appspot.com')
 
 #image has to be the same location as python script
 # Upload a local file to a new file to be created in your bucket.
-zebraBlob = bucket.blob('donk.jpg')
-zebraBlob.upload_from_filename(filename='donk.jpg')
+zebraBlob = bucket.blob('zebra.jpg')
+zebraBlob.upload_from_filename(filename='zebra.jpg')
+
 
 # Download a file from your bucket.
 #giraffeBlob = bucket.get_blob('giraffe.jpg')
